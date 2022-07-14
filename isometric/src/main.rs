a0585b13b4acf4f114eca15fa44f210b4819d8c6
Thomas Elder <tgelder@gmail.com>
2022-07-14 20:15:26 +0100

    WIP https://glium.github.io/glium/book/tuto-07-shape.html

--- isometric/src/main.rs
+++ isometric/src/main.rs
@@ -1,33 +1,120 @@
-use std::ffi::CStr;
+extern crate glium;
 
-mod gl {
-    include!(concat!(env!("OUT_DIR"), "/gl_bindings.rs"));
+use glium::{glutin, implement_vertex};
+use glium::{uniform, Surface};
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
 }
+
+implement_vertex!(Vertex, position);
 
 fn main() {
-    let event_loop = glutin::event_loop::EventLoop::new();
-    let window_builder = glutin::window::WindowBuilder::new()
-    .with_title("Isometric")
-    .with_inner_size(glutin::dpi::LogicalSize::new(1024.0, 768.0));
-    let windowed_context = glutin::ContextBuilder::new()
-    .build_windowed(window_builder, &event_loop)
-    .unwrap();
+    // 1. The **winit::EventsLoop** for handling events.
+    let mut event_loop = glium::glutin::event_loop::EventLoop::new();
+    // 2. Parameters for building the Window.
+    let wb = glium::glutin::window::WindowBuilder::new()
+        .with_inner_size(glium::glutin::dpi::LogicalSize::new(1024.0, 768.0))
+        .with_title("Hello world");
+    // 3. Parameters for building the OpenGL context.
+    let cb = glium::glutin::ContextBuilder::new();
+    // 4. Build the Display with the given window and OpenGL context parameters and register the
+    //    window with the events_loop.
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
-    let windowed_context = unsafe { windowed_context.make_current().unwrap() };
-
-
-    let context = windowed_context.context();
-
-    gl::load_with(|ptr| context.get_proc_address(ptr) as *const _);
-
-    let version = unsafe {
-        let data = CStr::from_ptr(gl::GetString(gl::VERSION) as *const _).to_bytes().to_vec();
-        String::from_utf8(data).unwrap()
+    let vertex1 = Vertex {
+        position: [-0.5, -0.5],
     };
+    let vertex2 = Vertex {
+        position: [0.0, 0.5],
+    };
+    let vertex3 = Vertex {
+        position: [0.5, -0.25],
+    };
+    let shape = vec![vertex1, vertex2, vertex3];
 
-    println!("OpenGL version {}", version);
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
 
-    loop {
-    }
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+            #version 130
+
+            in vec2 position;
+            out vec2 my_attr;
+
+            uniform mat4 matrix;
+
+            void main() {
+                my_attr = position;
+                gl_Position = matrix * vec4(position, 0.0, 1.0);
+            }
+        "#;
+
+    let fragment_shader_src = r#"
+            #version 130
+
+            in vec2 my_attr;
+            out vec4 color;
+
+            void main() {
+                color = vec4(my_attr, 0.0, 1.0);
+            }
+        "#;
+
+    let program =
+        glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
+            .unwrap();
+
+    let mut t: f32 = -0.5;
+    let mut i: f32 = 0.002;
+
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                }
+                _ => return,
+            },
+            glutin::event::Event::NewEvents(cause) => match cause {
+                glutin::event::StartCause::ResumeTimeReached { .. } => (),
+                glutin::event::StartCause::Init => (),
+                _ => return,
+            },
+            _ => return,
+        }
+        let next_frame_time =
+            std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+        t += i;
+        if !(-0.5..=0.5).contains(&t) {
+            i *= -1.0;
+        }
+        let uniforms = uniform! {
+            matrix: [
+                [ t.cos(), t.sin(), 0.0, 0.0],
+                [-t.sin(), t.cos(), 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0f32],
+            ]
+        };
+
+        let mut target = display.draw();
+
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target
+            .draw(
+                &vertex_buffer,
+                &indices,
+                &program,
+                &uniforms,
+                &Default::default(),
+            )
+            .unwrap();
+        target.finish().unwrap();
+    });
 }
-    
