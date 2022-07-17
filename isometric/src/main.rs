57900acbd4c9fcfe02f9428faea56c403bbf1488
Thomas Elder <tgelder@gmail.com>
2022-07-17 16:21:05 +0100

    PoC with terrain gen

--- isometric/src/main.rs
+++ isometric/src/main.rs
@@ -1,11 +1,16 @@
 extern crate glium;
 
+use std::f32::consts::PI;
+
+use commons::grid::Grid;
+use commons::noise::simplex_noise;
 use glium::{glutin, implement_vertex};
 use glium::{uniform, Surface};
+use terrain_gen::with_valleys::{heightmap_from_rises_with_valleys, ValleyParameters};
 
 #[derive(Copy, Clone)]
 struct Vertex {
-    position: [f32; 2],
+    position: [f32; 3],
 }
 
 implement_vertex!(Vertex, position);
@@ -23,43 +28,60 @@ fn main() {
     //    window with the events_loop.
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
-    let vertex1 = Vertex {
-        position: [-0.5, -0.5],
-    };
-    let vertex2 = Vertex {
-        position: [0.0, 0.5],
-    };
-    let vertex3 = Vertex {
-        position: [0.5, -0.25],
-    };
-    let shape = vec![vertex1, vertex2, vertex3];
+    let terrain = get_heightmap();
 
-    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let mut vertices =
+        Vec::with_capacity((terrain.width() - 1) as usize * (terrain.height() - 1) as usize * 6);
+
+    for x in 0..terrain.width() - 1 {
+        for y in 0..terrain.height() - 1 {
+            vertices.push(Vertex {
+                position: [x as f32, y as f32, terrain[(x, y)]],
+            });
+            vertices.push(Vertex {
+                position: [(x + 1) as f32, (y + 1) as f32, terrain[(x + 1, y + 1)]],
+            });
+            vertices.push(Vertex {
+                position: [(x + 1) as f32, y as f32, terrain[(x + 1, y)]],
+            });
+            vertices.push(Vertex {
+                position: [x as f32, y as f32, terrain[(x, y)]],
+            });
+            vertices.push(Vertex {
+                position: [x as f32, (y + 1) as f32, terrain[(x, y + 1)]],
+            });
+            vertices.push(Vertex {
+                position: [(x + 1) as f32, (y + 1) as f32, terrain[(x + 1, y + 1)]],
+            });
+        }
+    }
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &vertices).unwrap();
 
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
     let vertex_shader_src = r#"
             #version 130
 
-            in vec2 position;
-            out vec2 my_attr;
+            in vec3 position;
+            out float height;
 
             uniform mat4 matrix;
 
             void main() {
-                my_attr = position;
-                gl_Position = matrix * vec4(position, 0.0, 1.0);
+                height = position.z;
+                gl_Position = matrix * vec4(position.x, position.y, position.z * 32.0, 1.0);
             }
         "#;
 
     let fragment_shader_src = r#"
             #version 130
 
-            in vec2 my_attr;
+            in float height;
             out vec4 color;
 
             void main() {
-                color = vec4(my_attr, 0.0, 1.0);
+                color = vec4(height, height, height, 1.0);
             }
         "#;
 
@@ -94,12 +116,30 @@ fn main() {
         if !(-0.5..=0.5).contains(&t) {
             i *= -1.0;
         }
+        let scale = 1.0 / 128.0;
+        let yaw = PI / 4.0;
+        let pitch = PI / 4.0;
+        let yc = yaw.cos();
+        let ys = yaw.sin();
+        let pc = pitch.cos();
+        let ps = pitch.sin();
+        // na::Matrix4::from_vec(vec![
+        //     yc, ys, 0.0, 0.0,
+        //     -ys * pc, yc * pc, ps, 0.0,
+        //     0.0, 0.0, -1.0, 0.0,
+        //     0.0, 0.0, 0.0, 1.0,
+        // ])
+        // .transpose()
         let uniforms = uniform! {
             matrix: [
-                [ t.cos(), t.sin(), 0.0, 0.0],
-                [-t.sin(), t.cos(), 0.0, 0.0],
-                [0.0, 0.0, 1.0, 0.0],
-                [0.0, 0.0, 0.0, 1.0f32],
+                [scale * yc, scale * -ys * pc, 0.0, 0.0],
+                [scale * ys, scale * yc * pc, 0.0, 0.0],
+                [0.0, scale * ps, scale * -1.0, 0.0],
+                [-1.0, -0.0, 0.0, 1.0]
+                // [yc, ys, 0.0, 0.0],
+                // [-ys * pc, yc * pc, ps, 0.0],
+                // [0.0, 0.0, -1.0, 0.0],
+                // [0.0, 0.0, 0.0, 1.0],
             ]
         };
 
@@ -117,4 +157,24 @@ fn main() {
             .unwrap();
         target.finish().unwrap();
     });
+}
+
+fn get_heightmap() -> Grid<f32> {
+    let power = 8;
+    let weights = (0..power + 1)
+        .map(|i| 1.0f32 / 1.125f32.powf((power - i) as f32))
+        .collect::<Vec<_>>();
+    let rises = simplex_noise(power, 1987, &weights)
+        .normalize()
+        .map(|_, z| (0.5 - z).abs() / 0.5);
+
+    heightmap_from_rises_with_valleys(
+        &rises,
+        ValleyParameters {
+            height_threshold: 0.25,
+            rain_threshold: 128,
+            rise: 0.01,
+            origin_fn: |xy| rises.is_border(xy),
+        },
+    )
 }
