71209d209bc266a98eaca073ddf56e9e7efb9756
Thomas Elder <tgelder@gmail.com>
2023-04-09 11:06:20 +0100

    Fix

--- mountain/src/physics/mod.rs
+++ mountain/src/physics/mod.rs
@@ -3,8 +3,20 @@ use commons::quadratic;
 mod skiing;
 
 fn get_duration(acceleration: f32, velocity: f32, distance: f32) -> Option<f32> {
-    match quadratic::solve(acceleration / 2.0, velocity, -distance) {
-        quadratic::Solution::RealRoots { min: duration, .. } => Some(duration),
-        _ => None,
+    let quadratic::Solution::RealRoots { min, max } = quadratic::solve(acceleration / 2.0, velocity, -distance) else {return None};
+    if min >= 0.0 {
+        Some(min)
+    } else if max >= 0.0 {
+        Some(max)
+    } else {
+        None
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_name() {}
 }
