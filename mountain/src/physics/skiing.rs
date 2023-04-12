496a8eb8b7a35bf4c70cb1da60ae581c15680ce0
Thomas Elder <tgelder@gmail.com>
2023-04-12 14:17:20 +0100

    Minor changes

--- mountain/src/physics/skiing.rs
+++ mountain/src/physics/skiing.rs
@@ -1,18 +1,17 @@
 use std::f32::consts::PI;
 
-use commons::quadratic;
-
 use crate::physics::get_duration;
 
 const GRAVITY: f32 = 9.81;
 
-struct Solution {
+pub struct Solution {
     velocity: f32,
     duration: f32,
 }
 
-fn solve(run: f32, rise: f32, initial_velocity: f32) -> Option<Solution> {
-    let acceleration = get_acceleration(run, rise);
+// ski angle is 0 when skis are pointing downhill, PI / 2.0 when perpendicular to the slope
+pub fn solve(run: f32, rise: f32, initial_velocity: f32) -> Option<Solution> {
+    let acceleration = get_acceleration(run, rise)?;
     let distance = (run.powi(2) + rise.powi(2)).sqrt();
 
     let Some(duration) = get_duration(acceleration, initial_velocity, distance) else {return None};
@@ -22,8 +21,11 @@ fn solve(run: f32, rise: f32, initial_velocity: f32) -> Option<Solution> {
     Some(Solution { velocity, duration })
 }
 
-fn get_acceleration(run: f32, rise: f32) -> f32 {
+fn get_acceleration(run: f32, rise: f32) -> Option<f32> {
+    if rise == 0.0 {
+        return None;
+    }
     let angle = (rise / run).atan();
     let angle_ratio = angle / (PI / 2.0);
-    angle_ratio * GRAVITY
+    Some(angle_ratio * GRAVITY)
 }
