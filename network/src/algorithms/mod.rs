3d95d0bc6ca707e21d836687aad9457fac60f56a
Thomas Elder <tgelder@gmail.com>
2023-04-13 20:18:00 +0100

    Ready for review

--- network/src/algorithms/mod.rs
+++ network/src/algorithms/mod.rs
@@ -1,1 +1,27 @@
+use std::collections::{HashMap, HashSet};
+use std::hash::Hash;
+
+use crate::model::Edge;
+
+pub mod find_best_within_budget;
 pub mod find_path;
+
+fn get_path<T>(from: &HashSet<T>, focus: &T, entrances: &mut HashMap<T, Edge<T>>) -> Vec<Edge<T>>
+where
+    T: Copy + Eq + Hash,
+{
+    let mut out = vec![];
+    let mut focus = *focus;
+    while !from.contains(&focus) {
+        let entrance = entrances.remove(&focus);
+        match entrance {
+            Some(entrance) => {
+                focus = entrance.from;
+                out.push(entrance);
+            }
+            None => panic!("!"),
+        }
+    }
+    out.reverse();
+    out
+}
