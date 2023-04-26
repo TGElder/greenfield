c47bc9e5b295c98a2975a05038e7a3b1df8ac2a9
Thomas Elder <tgelder@gmail.com>
2023-04-26 20:38:11 +0100

    fix (mountain): skier acceleration was wrong

--- mountain/src/physics/skiing.rs
+++ mountain/src/physics/skiing.rs
@@ -25,6 +25,5 @@ fn get_acceleration(run: f32, rise: f32) -> Option<f32> {
         return None;
     }
     let angle = (-rise / run).atan();
-    let angle_ratio = angle / (PI / 2.0);
-    Some(angle_ratio * GRAVITY)
+    Some(angle.sin() * GRAVITY)
 }
