638065f7217daf4d9947181e27ee87185af2e126
TGElder <tgelder@gmail.com>
2023-06-19 20:49:21 +0100

    fix (mountain): selection now clears when building piste (#132)

--- mountain/src/handlers/piste_builder.rs
+++ mountain/src/handlers/piste_builder.rs
@@ -1,10 +1,10 @@
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 
-use commons::geometry::XYRectangle;
 use commons::origin_grid::OriginGrid;
 use engine::binding::Binding;
 
+use crate::handlers::selection;
 use crate::model::Piste;
 use crate::systems::overlay;
 
@@ -21,8 +21,8 @@ impl Handler {
     pub fn handle(
         &mut self,
         event: &engine::events::Event,
-        selection: &Option<XYRectangle<u32>>,
         pistes: &mut HashMap<usize, Piste>,
+        selection: &mut selection::Handler,
         overlay: &mut overlay::System,
     ) {
         let add = self.bindings.add.binds_event(event);
@@ -31,8 +31,8 @@ impl Handler {
             return;
         }
 
-        let Some(selection) = selection else {return};
-        let grid = OriginGrid::from_rectangle(selection, add);
+        let Some(rectangle) = selection.selected_rectangle() else {return};
+        let grid = OriginGrid::from_rectangle(rectangle, add);
 
         match pistes.entry(0) {
             Entry::Vacant(cell) => {
@@ -44,6 +44,7 @@ impl Handler {
             }
         };
 
-        overlay.update(*selection);
+        overlay.update(*rectangle);
+        selection.clear_selection();
     }
 }
--- mountain/src/handlers/selection.rs
+++ mountain/src/handlers/selection.rs
@@ -7,75 +7,79 @@ use super::*;
 
 pub struct Handler {
     pub origin: Option<XY<u32>>,
+    pub rectangle: Option<XYRectangle<u32>>,
     pub binding: Binding,
 }
 
 impl Handler {
+    pub fn new(binding: Binding) -> Handler {
+        Handler {
+            origin: None,
+            rectangle: None,
+            binding,
+        }
+    }
     pub fn handle(
         &mut self,
         event: &engine::events::Event,
         mouse_xy: &Option<XY<u32>>,
-        selection: &mut Option<XYRectangle<u32>>,
         graphics: &mut dyn engine::graphics::Graphics,
         overlay: &mut overlay::System,
     ) {
-        let previous_selection = *selection;
+        let previous_rectangle = self.rectangle;
 
         if let Event::MouseMoved(mouse_xy) = event {
-            self.modify_selection(selection, mouse_xy, graphics)
+            self.modify_selection(mouse_xy, graphics)
         }
 
         if self.binding.binds_event(event) {
             if self.origin.is_none() {
-                self.set_origin(mouse_xy, selection, graphics);
+                self.set_origin(mouse_xy, graphics);
             } else {
-                self.clear_selection(selection);
+                self.clear_selection();
             }
         }
 
-        if previous_selection != *selection {
-            previous_selection
+        let new_rectangle = self.rectangle;
+        if previous_rectangle != new_rectangle {
+            previous_rectangle
                 .iter()
                 .for_each(|update| overlay.update(*update));
-            selection.iter().for_each(|update| overlay.update(*update));
+            new_rectangle
+                .iter()
+                .for_each(|update| overlay.update(*update));
         }
     }
 
-    fn set_origin(
-        &mut self,
-        mouse_xy: &Option<XY<u32>>,
-        selection: &mut Option<XYRectangle<u32>>,
-        graphics: &mut dyn Graphics,
-    ) {
+    pub fn selected_rectangle(&self) -> &Option<XYRectangle<u32>> {
+        &self.rectangle
+    }
+
+    pub fn clear_selection(&mut self) {
+        self.rectangle = None;
+        self.origin = None;
+    }
+
+    fn set_origin(&mut self, mouse_xy: &Option<XY<u32>>, graphics: &mut dyn Graphics) {
         let Some(mouse_xy) = mouse_xy else {return};
         let Ok(xyz) = graphics.world_xyz_at(mouse_xy) else {return};
         let origin = selected_cell(xyz);
         self.origin = Some(origin);
-        *selection = Some(XYRectangle {
+        self.rectangle = Some(XYRectangle {
             from: origin,
             to: origin,
         });
     }
 
-    fn modify_selection(
-        &self,
-        selection: &mut Option<XYRectangle<u32>>,
-        mouse_xy: &XY<u32>,
-        graphics: &mut dyn Graphics,
-    ) {
+    fn modify_selection(&mut self, mouse_xy: &XY<u32>, graphics: &mut dyn Graphics) {
         let Some(origin) = self.origin else {return};
         let Ok(xyz) = graphics.world_xyz_at(mouse_xy) else {return};
         let focus = selected_cell(xyz);
 
-        *selection = Some(XYRectangle {
+        self.rectangle = Some(XYRectangle {
             from: xy(origin.x.min(focus.x), origin.y.min(focus.y)),
             to: xy(origin.x.max(focus.x), origin.y.max(focus.y)),
         });
-    }
-
-    fn clear_selection(&mut self, selection: &mut Option<XYRectangle<u32>>) {
-        self.origin = None;
-        *selection = None;
     }
 }
 
