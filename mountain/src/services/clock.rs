d096b25723ada8fcf432baf57b8fc8df4751f9a5
TGElder <tgelder@gmail.com>
2023-06-28 20:38:32 +0100

    fix (mountain): clock needs offset micros (#146)

--- mountain/src/services/clock.rs
+++ mountain/src/services/clock.rs
@@ -3,6 +3,7 @@ use std::time::Instant;
 pub struct Service {
     baseline: Instant,
     real_to_game: f32,
+    offset_micros: u128,
 }
 
 impl Service {
@@ -10,18 +11,26 @@ impl Service {
         Service {
             baseline: Instant::now(),
             real_to_game: 1.0,
+            offset_micros: 0,
         }
     }
 
     pub fn get_micros(&self) -> u128 {
-        Instant::now()
+        self.get_micros_at(&Instant::now())
+    }
+
+    fn get_micros_at(&self, instant: &Instant) -> u128 {
+        instant
             .duration_since(self.baseline)
             .mul_f32(self.real_to_game)
             .as_micros()
+            + self.offset_micros
     }
 
     pub fn _set_speed(&mut self, real_to_game: f32) {
-        self.baseline = Instant::now();
+        let new_baseline = Instant::now();
+        self.offset_micros = self.get_micros_at(&new_baseline);
+        self.baseline = new_baseline;
         self.real_to_game = real_to_game;
     }
 }
