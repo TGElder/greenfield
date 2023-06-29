008666e98ec7ca69e7cd406780136213b0447e0d
TGElder <tgelder@gmail.com>
2023-06-29 20:19:48 +0100

    feat (mountain): lift entry (#150)

---
+++ mountain/src/systems/lift_entry.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+
+use crate::model::skiing::{Plan, State};
+use crate::model::Lift;
+
+pub fn run(
+    plans: &HashMap<usize, Plan>,
+    targets: &HashMap<usize, usize>,
+    lifts: &HashMap<usize, Lift>,
+    locations: &mut HashMap<usize, usize>,
+) {
+    for (id, plan) in plans {
+        let Plan::Stationary(State{position, ..}) = plan else {continue};
+        let Some(target) = targets.get(id) else {continue};
+        match lifts.get(target) {
+            Some(Lift { from, .. }) if from == position => {
+                locations.insert(*id, *target);
+            }
+            _ => (),
+        }
+    }
+}
--- mountain/src/systems/mod.rs
+++ mountain/src/systems/mod.rs
@@ -1,5 +1,6 @@
 pub mod avatar_artist;
 pub mod cost_computer;
 pub mod framer;
+pub mod lift_entry;
 pub mod overlay;
 pub mod planner;
