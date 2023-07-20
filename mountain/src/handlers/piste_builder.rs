32e31854838257c5c87d098f66eabf8bca70b506
TGElder <tgelder@gmail.com>
2023-07-20 20:30:54 +0100

    feat (mountain): crude ability to build multiple pistes (#163)

--- mountain/src/handlers/piste_builder.rs
+++ mountain/src/handlers/piste_builder.rs
@@ -9,12 +9,14 @@ use crate::services::id_allocator;
 use crate::systems::overlay;
 
 pub struct Handler {
+    pub id: usize,
     pub bindings: Bindings,
 }
 
 pub struct Bindings {
     pub add: Binding,
     pub subtract: Binding,
+    pub new: Binding,
 }
 
 impl Handler {
@@ -26,7 +28,7 @@ impl Handler {
         overlay: &mut overlay::System,
         id_allocator: &mut id_allocator::Service,
     ) {
-        let add = self.bindings.add.binds_event(event);
+        let add = self.bindings.add.binds_event(event) || self.bindings.new.binds_event(event);
         let subtract = self.bindings.subtract.binds_event(event);
         if !(add || subtract) {
             return;
@@ -35,12 +37,12 @@ impl Handler {
         let Some(rectangle) = selection.selected_rectangle() else {return};
         let grid = OriginGrid::from_rectangle(rectangle, add);
 
-        if pistes.is_empty() {
-            pistes.insert(id_allocator.next_id(), Piste { grid });
+        if pistes.is_empty() || self.bindings.new.binds_event(event) {
+            self.id = id_allocator.next_id();
+            println!("Building piste {}", self.id);
+            pistes.insert(self.id, Piste { grid });
         } else {
-            pistes.iter_mut().for_each(|(_, piste)| {
-                piste.grid = piste.grid.paste(&grid);
-            });
+            pistes.get_mut(&self.id).unwrap().grid = pistes[&self.id].grid.paste(&grid);
         };
 
         overlay.update(*rectangle);
