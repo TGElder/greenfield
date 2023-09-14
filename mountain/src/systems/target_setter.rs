06eb884bd88d9d9eb5017f94805ffead7d4b26a2
TGElder <tgelder@gmail.com>
2023-09-14 20:08:30 +0100

    feat (mountain): crude downhill-only target setting (#192)

--- mountain/src/systems/target_setter.rs
+++ mountain/src/systems/target_setter.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+use commons::grid::Grid;
 use rand::seq::SliceRandom;
 
 use crate::model::lift::Lift;
@@ -7,13 +8,17 @@ use crate::model::piste::Piste;
 use crate::model::skiing::Plan;
 
 pub fn run(
+    terrain: &Grid<f32>,
     plans: &HashMap<usize, Plan>,
     locations: &HashMap<usize, usize>,
     pistes: &HashMap<usize, Piste>,
     lifts: &HashMap<usize, Lift>,
     targets: &mut HashMap<usize, usize>,
 ) {
-    for id in plans.keys() {
+    for (id, plan) in plans {
+        let Plan::Stationary(state) = plan else {
+            continue;
+        };
         if targets.contains_key(id) {
             continue;
         }
@@ -24,10 +29,13 @@ pub fn run(
             continue;
         };
 
+        let elevation = terrain[state.position];
+
         let candidates = lifts
             .iter()
             .filter(|(_, lift)| piste.grid.in_bounds(lift.from))
             .filter(|(_, lift)| piste.grid[lift.from])
+            .filter(|(_, lift)| terrain[lift.from] < elevation)
             .map(|(id, _)| *id)
             .collect::<Vec<_>>();
         let choice = candidates.choose(&mut rand::thread_rng());
