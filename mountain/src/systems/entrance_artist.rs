ed6c3d00c8cc0ec027b9727d28ca53dcc29b48ea
TGElder <tgelder@gmail.com>
2023-12-05 20:50:09 +0000

    feat (mountain): yellow shows entrance directon (#248)

--- mountain/src/systems/entrance_artist.rs
+++ mountain/src/systems/entrance_artist.rs
@@ -11,12 +11,13 @@ pub fn run(
     graphics: &mut dyn Graphics,
     entrances: &HashMap<usize, Entrance>,
     terrain: &Grid<f32>,
+    piste_map: &Grid<Option<usize>>,
     drawings: &mut HashMap<usize, usize>,
 ) {
     for (id, entrance) in entrances {
         if let Entry::Vacant(cell) = drawings.entry(*id) {
             if let Ok(index) = graphics.create_quads() {
-                draw(graphics, &index, entrance, terrain);
+                draw(graphics, &index, entrance, terrain, piste_map);
                 cell.insert(index);
             }
         };
