b4a60fc33f36e351b25e23a47862efa7df7bcc14
Thomas Elder <tgelder@gmail.com>
2023-12-12 20:17:38 +0000

    feat (mountain): open status for lifts + lift opener

---
+++ mountain/src/handlers/lift_opener.rs
@@ -0,0 +1,42 @@
+use std::collections::{HashMap, HashSet};
+
+use commons::geometry::{xy, XY, XYZ};
+use engine::binding::Binding;
+
+use crate::model::lift::Lift;
+
+pub struct Handler {
+    pub binding: Binding,
+}
+
+impl Handler {
+    pub fn handle(
+        &self,
+        event: &engine::events::Event,
+        mouse_xy: &Option<XY<u32>>,
+        lifts: &HashMap<usize, Lift>,
+        open: &mut HashSet<usize>,
+        graphics: &mut dyn engine::graphics::Graphics,
+    ) {
+        if !self.binding.binds_event(event) {
+            return;
+        }
+
+        let Some(mouse_xy) = mouse_xy else { return };
+        let Ok(XYZ { x, y, .. }) = graphics.world_xyz_at(mouse_xy) else {
+            return;
+        };
+        let position = xy(x.round() as u32, y.round() as u32);
+
+        for (lift_id, lift) in lifts {
+            if lift.pick_up.position == position || lift.drop_off.position == position {
+                if open.remove(lift_id) {
+                    println!("Lift {} is closed", lift_id);
+                } else {
+                    open.insert(*lift_id);
+                    println!("Lift {} is open", lift_id);
+                }
+            }
+        }
+    }
+}
