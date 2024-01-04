c27e719037c97817a67be0cc3019d6c5372fc8b0
TGElder <tgelder@gmail.com>
2024-01-04 19:29:39 +0000

    fix (mountain): skiers are no longer set impossible targets (#271)

--- mountain/src/systems/target_setter.rs
+++ mountain/src/systems/target_setter.rs
@@ -1,16 +1,14 @@
 use std::collections::{HashMap, HashSet};
 
-use commons::grid::Grid;
 use rand::seq::SliceRandom;
 
-use crate::model::exit::Exit;
+use crate::model::piste::Basins;
 use crate::model::skiing::Plan;
 
 pub fn run(
-    terrain: &Grid<f32>,
     plans: &HashMap<usize, Plan>,
     locations: &HashMap<usize, usize>,
-    exits: &HashMap<usize, Vec<Exit>>,
+    basins: &HashMap<usize, Basins>,
     open: &HashSet<usize>,
     targets: &mut HashMap<usize, usize>,
 ) {
@@ -18,31 +16,36 @@ pub fn run(
         let Plan::Stationary(state) = plan else {
             continue;
         };
-        if targets.contains_key(plan_id) {
-            continue;
-        }
+
         let Some(location_id) = locations.get(plan_id) else {
             continue;
         };
 
-        let elevation = terrain[state.position];
+        let Some(basins) = basins.get(location_id) else {
+            continue;
+        };
 
-        let candidates = exits
-            .get(location_id)
-            .into_iter()
-            .flatten()
-            .filter(|Exit { id, .. }| open.contains(id))
-            .filter(|Exit { positions, .. }| {
-                positions
-                    .iter()
-                    .any(|position| terrain[position] <= elevation)
-            })
-            .map(|Exit { id, .. }| *id)
+        let candidates = basins
+            .targets_reachable_from_state(state)
+            .filter(|target| open.contains(target))
             .collect::<Vec<_>>();
+
+        if let Some(current_target) = targets.get(plan_id) {
+            if !candidates.contains(&current_target) {
+                println!(
+                    "INFO: Removing invalid target {} from {}",
+                    current_target, plan_id
+                );
+                targets.remove(plan_id);
+            } else {
+                continue;
+            }
+        }
+
         let choice = candidates.choose(&mut rand::thread_rng());
 
         if let Some(choice) = choice {
-            targets.insert(*plan_id, *choice);
+            targets.insert(*plan_id, **choice);
         }
     }
 }
