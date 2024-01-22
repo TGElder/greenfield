5802c97920947448ad33aeb604ca018cb8fa68d1
Thomas Elder <tgelder@gmail.com>
2024-01-22 20:36:13 +0000

    feat (terrain_gen)!:  heightmap_from_rises output no longer normalized

--- mountain/src/init/terrain.rs
+++ mountain/src/init/terrain.rs
@@ -6,11 +6,11 @@ use terrain_gen::heightmap_from_rises;
 pub fn generate_heightmap() -> Grid<f32> {
     let power = 9;
     let weights = (0..power + 1)
-        .map(|i| 1.0f32 / 1.125f32.powf((power - i) as f32))
+        .map(|i| 1.0f32 / 1.4f32.powf((power - i) as f32))
         .collect::<Vec<_>>();
     let rises = simplex_noise(power, 1987, &weights)
         .normalize()
         .map(|_, z| (0.5 - z).abs() / 0.5);
 
-    heightmap_from_rises(&rises, |xy| rises.is_border(xy)).map(|_, v| v * 128.0)
+    heightmap_from_rises(&rises, |xy| xy.x == 0 && xy.y == 0)
 }
