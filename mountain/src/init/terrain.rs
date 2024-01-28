65d47e2db012260654bf54736e7ab4b23d831208
Thomas Elder <tgelder@gmail.com>
2024-01-28 19:24:13 +0000

    feat (mountain): better heightmap

--- mountain/src/init/terrain.rs
+++ mountain/src/init/terrain.rs
@@ -1,16 +1,22 @@
 use commons::grid::Grid;
 use commons::noise::simplex_noise;
 
+use commons::scale::Scale;
 use terrain_gen::heightmap_from_rises;
 
 pub fn generate_heightmap() -> Grid<f32> {
     let power = 10;
     let weights = (0..power + 1)
-        .map(|i| 1.0f32 / 1.35f32.powf((power - i) as f32))
+        .map(|i| 1.0f32 / 1.4f32.powf((power - i) as f32))
         .collect::<Vec<_>>();
-    let rises = simplex_noise(power, 1987, &weights)
+    let rises = simplex_noise(power, 1989, &weights)
         .normalize()
         .map(|_, z| (0.5 - z).abs() / 0.5);
 
-    heightmap_from_rises(&rises, |xy| xy.x == 0 && xy.y == 0)
+    let minimum_slope = 0.1;
+    let maximum_slope = 1.0;
+    let scale = Scale::new((0.0, 1.0), (minimum_slope, maximum_slope));
+    let rises = rises.map(|_, z| scale.scale(z));
+
+    heightmap_from_rises(&rises, |xy| xy.x == 0)
 }
