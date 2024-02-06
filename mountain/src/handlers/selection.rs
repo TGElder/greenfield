e3d6cc69f7e1846afb6c74731212ce48d35b7222
Thomas Elder <tgelder@gmail.com>
2024-02-06 20:41:49 +0000

    Mostly working PoC

--- mountain/src/handlers/selection.rs
+++ mountain/src/handlers/selection.rs
@@ -1,8 +1,9 @@
 use commons::geometry::{xy, XYRectangle, XY, XYZ};
-use commons::grid::Grid;
+use commons::grid::{Grid, CORNERS, OFFSETS_4};
 use commons::origin_grid::OriginGrid;
 use engine::binding::Binding;
 use engine::events::Event;
+use line_drawing::Bresenham;
 
 use crate::systems::overlay;
 
@@ -132,30 +133,76 @@ impl Handler {
         let Some(circle_end) = self.circle_end else {
             return;
         };
-        let circle_start = xy(circle_start.x as f32 + 0.5, circle_start.y as f32 + 0.5);
-        let circle_end = xy(circle_end.x as f32 + 0.5, circle_end.y as f32 + 0.5);
-        let center = (circle_start + circle_end) / 2.0;
-        let radius = (circle_start - circle_end).magnitude() / 2.0;
-        let grid = OriginGrid::from_rectangle(
-            XYRectangle {
-                from: xy(
-                    (center.x - radius).floor() as u32,
-                    (center.y - radius).floor() as u32,
-                ),
-                to: xy(
-                    (center.x + radius).ceil() as u32,
-                    (center.y + radius).ceil() as u32,
-                ),
-            },
-            true,
-        );
-        let radius_squared = radius.powf(2.0) + 0.25;
-        let grid = grid.map(|position, _| {
-            ((position.x as f32 + 0.5) - center.x).powf(2.0)
-                + ((position.y as f32 + 0.5) - center.y).powf(2.0)
-                <= radius_squared
-        });
-        self.grid = Some(grid)
+        if let Some(target) = self.target {
+            let b = project_point_onto_line(&target, (&circle_start, &circle_end));
+            let d = xy(target.x as i32, target.y as i32)
+                - (xy(b.x as i32, b.y as i32) - xy(circle_start.x as i32, circle_start.y as i32));
+            let d = xy(d.x as u32, d.y as u32);
+            let mut grid = OriginGrid::from_rectangle(
+                XYRectangle {
+                    from: xy(
+                        circle_start.x.min(b.x).min(target.x).min(d.x),
+                        circle_start.y.min(b.y).min(target.y).min(d.y),
+                    ),
+                    to: xy(
+                        circle_start.x.max(b.x).max(target.x).max(d.x),
+                        circle_start.y.max(b.y).max(target.y).max(d.y),
+                    ),
+                },
+                false,
+            );
+            for (x, y) in Bresenham::new(
+                (circle_start.x as i32, circle_start.y as i32),
+                (b.x as i32, b.y as i32),
+            ) {
+                let position = xy(x as u32, y as u32);
+                grid[position] = true;
+            }
+            for (x, y) in
+                Bresenham::new((b.x as i32, b.y as i32), (target.x as i32, target.y as i32))
+            {
+                let position = xy(x as u32, y as u32);
+                grid[position] = true;
+            }
+            for (x, y) in
+                Bresenham::new((target.x as i32, target.y as i32), (d.x as i32, d.y as i32))
+            {
+                let position = xy(x as u32, y as u32);
+                grid[position] = true;
+            }
+            for (x, y) in Bresenham::new(
+                (d.x as i32, d.y as i32),
+                (circle_start.x as i32, circle_start.y as i32),
+            ) {
+                let position = xy(x as u32, y as u32);
+                grid[position] = true;
+            }
+            let center = (circle_start + target) / 2;
+            flood_fill(center, &mut grid);
+            self.grid = Some(grid)
+        } else {
+            let mut grid = OriginGrid::from_rectangle(
+                XYRectangle {
+                    from: xy(
+                        circle_start.x.min(circle_end.x),
+                        circle_start.y.min(circle_end.y),
+                    ),
+                    to: xy(
+                        circle_start.x.max(circle_end.x),
+                        circle_start.y.max(circle_end.y),
+                    ),
+                },
+                false,
+            );
+            for (x, y) in Bresenham::new(
+                (circle_start.x as i32, circle_start.y as i32),
+                (circle_end.x as i32, circle_end.y as i32),
+            ) {
+                let position = xy(x as u32, y as u32);
+                grid[position] = true;
+            }
+            self.grid = Some(grid)
+        };
     }
 }
 
@@ -164,4 +211,32 @@ fn selected_cell(terrain: &Grid<f32>, XYZ { x, y, .. }: XYZ<f32>) -> XY<u32> {
         (x.floor() as u32).min(terrain.width() - 2),
         (y.floor() as u32).min(terrain.height() - 2),
     )
+}
+
+fn project_point_onto_line(point: &XY<u32>, (from, to): (&XY<u32>, &XY<u32>)) -> XY<u32> {
+    let e1 = xy(to.x as f32 - from.x as f32, to.y as f32 - from.y as f32);
+    let e2 = xy(
+        point.x as f32 - from.x as f32,
+        point.y as f32 - from.y as f32,
+    );
+    let dot_product = e1.x * e2.x + e1.y * e2.y;
+    let cos_a = dot_product / (e1.magnitude() * e2.magnitude());
+    let v1_p_magnitude = cos_a * e2.magnitude();
+    let p = v1_p_magnitude / e1.magnitude();
+    let out = xy(from.x as f32 + p * e1.x, from.y as f32 + p * e1.y);
+    xy(out.x.round() as u32, out.y.round() as u32)
+}
+
+fn flood_fill(position: XY<u32>, grid: &mut OriginGrid<bool>) {
+    let mut queue = Vec::with_capacity((grid.width() * grid.height()) as usize);
+    grid[position] = true;
+    queue.push(position);
+    while let Some(position) = queue.pop() {
+        for corner in grid.offsets(position, &OFFSETS_4).collect::<Vec<_>>() {
+            if !grid[corner] {
+                grid[corner] = true;
+                queue.push(corner);
+            }
+        }
+    }
 }
