07bc3d7229318df8267cc4a08c1fee034b93038b
Thomas Elder <tgelder@gmail.com>
2024-02-12 20:24:05 +0000

    WIP

--- mountain/src/init/terrain.rs
+++ mountain/src/init/terrain.rs
@@ -5,18 +5,24 @@ use commons::scale::Scale;
 use terrain_gen::heightmap_from_rises;
 
 pub fn generate_heightmap() -> Grid<f32> {
-    let power = 10;
+    let power = 11;
+    let seed = 2001;
     let weights = (0..power + 1)
-        .map(|i| 1.0f32 / 1.4f32.powf((power - i) as f32))
+        .map(|i| if i > 10 {0.0}else{1.0f32 / 1.4f32.powf((power - i) as f32)})
+        // .map(|i| if i == power {0.0}else{1.0f32 / 1.4f32.powf((power - i) as f32)})
         .collect::<Vec<_>>();
-    let rises = simplex_noise(power, 1989, &weights)
+    let rises = simplex_noise(power, seed, &weights)
         .normalize()
         .map(|_, z| (0.5 - z).abs() / 0.5);
 
-    let minimum_slope = 0.08;
-    let maximum_slope = 1.0;
-    let scale = Scale::new((0.0, 1.0), (minimum_slope, maximum_slope));
-    let rises = rises.map(|_, z| scale.scale(z));
+    let minimum_weights = (0..power + 1)
+        .map(|i| 1.0f32 / 2.0f32.powf((power - i) as f32))
+        .collect::<Vec<_>>();
+    let minimums = simplex_noise(power, seed + 1, &minimum_weights)
+        .normalize()
+        .map(|_, z| (0.5 - z).abs() / 0.5);
 
-    heightmap_from_rises(&rises, |xy| xy.x == 0)
+    let rises = rises.map(|xy, z| Scale::new((0.0, 1.0), (0.05 + minimums[xy] * 0.15, 1.0)).scale(z));
+
+    heightmap_from_rises(&rises, |xy| xy.x == 0 || xy.y == 0)
 }
