fcc36412d5de1f0d53966538e37ff9b4c90b39d6
Thomas Elder <tgelder@gmail.com>
2024-02-15 20:21:27 +0000

    Self review

--- mountain/src/draw/frame.rs
+++ mountain/src/draw/frame.rs
@@ -9,14 +9,6 @@ use crate::draw::model;
 use crate::model::frame::{Frame, Model};
 
 pub fn draw(graphics: &mut dyn Graphics, index: &usize, frame: &Frame) {
-    let translation: Matrix4<f32> = [
-        [1.0, 0.0, 0.0, 0.0],
-        [0.0, 1.0, 0.0, 0.0],
-        [0.0, 0.0, 1.0, 0.0],
-        [frame.position.x, frame.position.y, frame.position.z, 1.0],
-    ]
-    .into();
-
     let cos = frame.angle.cos();
     let sin = frame.angle.sin();
     let rotation: Matrix4<f32> = [
@@ -24,6 +16,14 @@ pub fn draw(graphics: &mut dyn Graphics, index: &usize, frame: &Frame) {
         [-sin, cos, 0.0, 0.0],
         [0.0, 0.0, 1.0, 0.0],
         [0.0, 0.0, 0.0, 1.0],
+    ]
+    .into();
+
+    let translation: Matrix4<f32> = [
+        [1.0, 0.0, 0.0, 0.0],
+        [0.0, 1.0, 0.0, 0.0],
+        [0.0, 0.0, 1.0, 0.0],
+        [frame.position.x, frame.position.y, frame.position.z, 1.0],
     ]
     .into();
 
--- mountain/src/draw/model/tree.rs
+++ mountain/src/draw/model/tree.rs
@@ -7,9 +7,9 @@ const GREEN: Rgb<f32> = Rgb::new(0.0, 0.373, 0.337);
 const BROWN: Rgb<f32> = Rgb::new(0.447, 0.361, 0.259);
 
 pub fn model() -> Vec<Triangle> {
-    pyramid::model(1.5, 7.0, 10.0, GREEN)
+    pyramid::model(0.5, 0.0, 10.0, BROWN)
         .into_iter()
         .chain(pyramid::model(2.0, 4.0, 8.0, GREEN))
-        .chain(pyramid::model(0.5, 0.0, 10.0, BROWN))
+        .chain(pyramid::model(1.5, 7.0, 10.0, GREEN))
         .collect()
 }
