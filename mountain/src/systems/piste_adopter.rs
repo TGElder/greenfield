8a2aae0a56692974a95cb2811f09b228d2a87647
Thomas Elder <tgelder@gmail.com>
2024-04-18 19:51:27 +0100

    fix (mountain): piste adopter considers ability

--- mountain/src/systems/piste_adopter.rs
+++ mountain/src/systems/piste_adopter.rs
@@ -1,14 +1,21 @@
 use std::collections::HashMap;
 
 use commons::grid::{Grid, CORNERS_INVERSE};
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 
+use crate::model::ability::Ability;
+use crate::model::skier::Skier;
 use crate::model::skiing::{Plan, State};
 
 pub fn run(
     plans: &HashMap<usize, Plan>,
     piste_map: &Grid<Option<usize>>,
+    skiers: &HashMap<usize, Skier>,
+    abilities: &HashMap<usize, Ability>,
     locations: &mut HashMap<usize, usize>,
 ) {
+    let mut rng = thread_rng();
     for (id, plan) in plans.iter() {
         let Plan::Stationary(State { position, .. }) = plan else {
             continue;
@@ -23,12 +30,28 @@ pub fn run(
             }
         }
 
-        let piste = piste_map
+        let Some(Skier {
+            ability: skier_ability,
+            ..
+        }) = skiers.get(id)
+        else {
+            continue;
+        };
+
+        let candidates = piste_map
             .offsets(position, &CORNERS_INVERSE)
             .flat_map(|cell| piste_map[cell])
-            .next();
+            .filter(|piste| {
+                abilities
+                    .get(piste)
+                    .map(|piste_ability| piste_ability <= skier_ability)
+                    .unwrap_or_default()
+            })
+            .collect::<Vec<_>>();
 
-        if let Some(piste) = piste {
+        let piste = candidates.choose(&mut rng);
+
+        if let Some(&piste) = piste {
             locations.insert(*id, piste);
         }
     }
