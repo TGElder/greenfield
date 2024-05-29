bef92a114e8cffa008ddb56077b5d72247c3e3d4
Thomas Elder <tgelder@gmail.com>
2024-05-29 20:23:27 +0100

    feat (mountain): door direction

--- mountain/src/model/door.rs
+++ mountain/src/model/door.rs
@@ -3,10 +3,13 @@ use std::collections::HashSet;
 use commons::geometry::{XYRectangle, XY};
 use serde::{Deserialize, Serialize};
 
+use crate::model::direction::Direction;
+
 #[derive(Serialize, Deserialize)]
 pub struct Door {
     pub building_id: usize,
     pub piste_id: usize,
     pub footprint: XYRectangle<u32>,
     pub aperture: HashSet<XY<u32>>,
+    pub direction: Direction,
 }
