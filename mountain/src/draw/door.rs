9c9ed11698cbe9c01b891b812d2126de3348932f
Thomas Elder <tgelder@gmail.com>
2024-05-29 20:35:40 +0100

    feat (mountain): black doorways

--- mountain/src/draw/door.rs
+++ mountain/src/draw/door.rs
@@ -9,9 +9,11 @@ use engine::graphics::transform::{Recolor, Transform};
 use engine::graphics::utils::{transformation_matrix, triangles_from_quads, Transformation};
 use engine::graphics::Graphics;
 
+use crate::model::direction::Direction;
 use crate::model::door::Door;
 
-const COLOR: Rgb<f32> = Rgb::new(0.447, 0.361, 0.259);
+const STRUCTURE_COLOR: Rgb<f32> = Rgb::new(0.447, 0.361, 0.259);
+const APERTURE_COLOR: Rgb<f32> = Rgb::new(0.0, 0.0, 0.0);
 const HEIGHT: f32 = 2.0;
 
 pub fn draw(graphics: &mut dyn Graphics, index: &usize, door: &Door, terrain: &Grid<f32>) {
@@ -40,7 +42,9 @@ pub fn draw(graphics: &mut dyn Graphics, index: &usize, door: &Door, terrain: &G
 
     let origin = xyz(from.x, from.y, min_ground_height);
     let scale = xyz(to.x - from.x, to.y - from.y, total_height);
-    let quads = translated_and_scaled_cube(origin + scale / 2.0, scale, COLOR).collect::<Vec<_>>();
+    let quads =
+        translated_and_scaled_cube(origin + scale / 2.0, scale, &|side| coloring(side, door))
+            .collect::<Vec<_>>();
 
     let triangles = triangles_from_quads(&quads);
     graphics.draw_triangles(index, &triangles).unwrap();
@@ -68,10 +72,28 @@ where
     }
 }
 
+fn coloring(side: &cube::Side, door: &Door) -> Rgb<f32> {
+    if aperture_side(door.direction) == Some(*side) {
+        APERTURE_COLOR
+    } else {
+        STRUCTURE_COLOR
+    }
+}
+
+fn aperture_side(direction: Direction) -> Option<cube::Side> {
+    match direction {
+        Direction::East => Some(cube::Side::Right),
+        Direction::North => Some(cube::Side::Front),
+        Direction::West => Some(cube::Side::Left),
+        Direction::South => Some(cube::Side::Back),
+        _ => None,
+    }
+}
+
 fn translated_and_scaled_cube(
     translation: XYZ<f32>,
     scale: XYZ<f32>,
-    color: Rgb<f32>,
+    coloring: &dyn Fn(&cube::Side) -> Rgb<f32>,
 ) -> impl Iterator<Item = Quad<Rgb<f32>>> {
     let transformation = transformation_matrix(Transformation {
         translation: Some(translation),
@@ -81,6 +103,6 @@ fn translated_and_scaled_cube(
 
     cube::model()
         .transform(&transformation)
-        .recolor(&|_| color)
+        .recolor(coloring)
         .into_iter()
 }
