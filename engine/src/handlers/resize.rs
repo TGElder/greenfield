9f18708be962f9f3e73345d2c1e14098274d3a92
TGElder <tgelder@gmail.com>
2024-08-27 20:13:55 +0100

    refactor (engine): bindings now part of handler calls (#435)

--- engine/src/handlers/resize.rs
+++ engine/src/handlers/resize.rs
@@ -1,22 +1,7 @@
-use crate::{
-    engine::Engine,
-    events::{Event, EventHandler},
-    graphics::Graphics,
-};
+use crate::{engine::Engine, events::Event, graphics::Graphics};
 
-#[derive(Default)]
-pub struct Handler {}
-
-impl Handler {
-    pub fn new() -> Handler {
-        Handler {}
-    }
-}
-
-impl EventHandler for Handler {
-    fn handle(&mut self, event: &Event, _: &mut dyn Engine, graphics: &mut dyn Graphics) {
-        if let Event::WindowResize(rectangle) = event {
-            graphics.projection().set_viewport(*rectangle);
-        }
+pub fn handle(event: &Event, _: &mut dyn Engine, graphics: &mut dyn Graphics) {
+    if let Event::WindowResize(rectangle) = event {
+        graphics.projection().set_viewport(*rectangle);
     }
 }
