85ddce5c91092ea178020003096c24a03b98fe05
Thomas Elder <tgelder@gmail.com>
2024-09-18 20:58:33 +0100

    fix (engine): shutdown call was ignored

--- engine/src/glium_backend/engine.rs
+++ engine/src/glium_backend/engine.rs
@@ -70,114 +70,122 @@ where
         let mut cursor_position: Option<winit::dpi::PhysicalPosition<f64>> = None;
 
         self.event_loop
-            .run(move |event, window_target| match event {
-                winit::event::Event::NewEvents(
-                    winit::event::StartCause::Init
-                    | winit::event::StartCause::ResumeTimeReached { .. },
-                ) => {
-                    let next_frame_time =
-                        std::time::Instant::now() + self.parameters.frame_duration;
-                    window_target.set_control_flow(winit::event_loop::ControlFlow::WaitUntil(
-                        next_frame_time,
-                    ));
-
-                    if let Some(position) = cursor_position {
-                        let (x, y) = position.into();
-                        self.event_handler.handle(
-                            &Event::MouseMoved(xy(x, y)),
-                            &mut self.state,
-                            &mut self.graphics,
-                        );
-                    }
-
-                    self.event_handler
-                        .handle(&Event::Tick, &mut self.state, &mut self.graphics);
-
-                    cursor_position = None;
-
-                    match self.graphics.render() {
-                        Ok(_) => (),
-                        Err(err) => println!("Failed to render frame: {err}"),
-                    };
+            .run(move |event, window_target| {
+                if !self.state.running {
+                    window_target.exit();
                 }
-                winit::event::Event::WindowEvent { event, .. } => {
-                    if self.graphics.handle_window_event(&event).consumed {
-                        return;
-                    }
+                match event {
+                    winit::event::Event::NewEvents(
+                        winit::event::StartCause::Init
+                        | winit::event::StartCause::ResumeTimeReached { .. },
+                    ) => {
+                        let next_frame_time =
+                            std::time::Instant::now() + self.parameters.frame_duration;
+                        window_target.set_control_flow(winit::event_loop::ControlFlow::WaitUntil(
+                            next_frame_time,
+                        ));
 
-                    match event {
-                        winit::event::WindowEvent::CloseRequested => {
-                            window_target.exit();
-                        }
-                        winit::event::WindowEvent::CursorMoved { position, .. } => {
-                            cursor_position = Some(position);
-                        }
-                        winit::event::WindowEvent::MouseInput { button, state, .. } => {
+                        if let Some(position) = cursor_position {
+                            let (x, y) = position.into();
                             self.event_handler.handle(
-                                &Event::Button {
-                                    button: button.into(),
-                                    state: state.into(),
-                                },
+                                &Event::MouseMoved(xy(x, y)),
                                 &mut self.state,
                                 &mut self.graphics,
                             );
                         }
-                        winit::event::WindowEvent::MouseWheel {
-                            delta: winit::event::MouseScrollDelta::LineDelta(_, y),
-                            ..
-                        } => {
-                            if y > 0.0 {
+
+                        self.event_handler.handle(
+                            &Event::Tick,
+                            &mut self.state,
+                            &mut self.graphics,
+                        );
+
+                        cursor_position = None;
+
+                        match self.graphics.render() {
+                            Ok(_) => (),
+                            Err(err) => println!("Failed to render frame: {err}"),
+                        };
+                    }
+                    winit::event::Event::WindowEvent { event, .. } => {
+                        if self.graphics.handle_window_event(&event).consumed {
+                            return;
+                        }
+
+                        match event {
+                            winit::event::WindowEvent::CloseRequested => {
+                                window_target.exit();
+                            }
+                            winit::event::WindowEvent::CursorMoved { position, .. } => {
+                                cursor_position = Some(position);
+                            }
+                            winit::event::WindowEvent::MouseInput { button, state, .. } => {
                                 self.event_handler.handle(
                                     &Event::Button {
-                                        button: Button::Mouse(MouseButton::WheelUp),
-                                        state: ButtonState::Pressed,
-                                    },
-                                    &mut self.state,
-                                    &mut self.graphics,
-                                );
-                            } else if y < 0.0 {
-                                self.event_handler.handle(
-                                    &Event::Button {
-                                        button: Button::Mouse(MouseButton::WheelDown),
-                                        state: ButtonState::Pressed,
+                                        button: button.into(),
+                                        state: state.into(),
                                     },
                                     &mut self.state,
                                     &mut self.graphics,
                                 );
                             }
+                            winit::event::WindowEvent::MouseWheel {
+                                delta: winit::event::MouseScrollDelta::LineDelta(_, y),
+                                ..
+                            } => {
+                                if y > 0.0 {
+                                    self.event_handler.handle(
+                                        &Event::Button {
+                                            button: Button::Mouse(MouseButton::WheelUp),
+                                            state: ButtonState::Pressed,
+                                        },
+                                        &mut self.state,
+                                        &mut self.graphics,
+                                    );
+                                } else if y < 0.0 {
+                                    self.event_handler.handle(
+                                        &Event::Button {
+                                            button: Button::Mouse(MouseButton::WheelDown),
+                                            state: ButtonState::Pressed,
+                                        },
+                                        &mut self.state,
+                                        &mut self.graphics,
+                                    );
+                                }
+                            }
+                            winit::event::WindowEvent::KeyboardInput {
+                                event:
+                                    winit::event::KeyEvent {
+                                        logical_key: key,
+                                        state,
+                                        ..
+                                    },
+                                ..
+                            } => {
+                                self.event_handler.handle(
+                                    &Event::Button {
+                                        button: key.into(),
+                                        state: state.into(),
+                                    },
+                                    &mut self.state,
+                                    &mut self.graphics,
+                                );
+                            }
+                            winit::event::WindowEvent::Resized(winit::dpi::PhysicalSize {
+                                width,
+                                height,
+                            }) => {
+                                self.event_handler.handle(
+                                    &Event::WindowResize(Rectangle { width, height }),
+                                    &mut self.state,
+                                    &mut self.graphics,
+                                );
+                            }
+                            _ => (),
                         }
-                        winit::event::WindowEvent::KeyboardInput {
-                            event:
-                                winit::event::KeyEvent {
-                                    logical_key: key,
-                                    state,
-                                    ..
-                                },
-                            ..
-                        } => {
-                            self.event_handler.handle(
-                                &Event::Button {
-                                    button: key.into(),
-                                    state: state.into(),
-                                },
-                                &mut self.state,
-                                &mut self.graphics,
-                            );
-                        }
-                        winit::event::WindowEvent::Resized(winit::dpi::PhysicalSize {
-                            width,
-                            height,
-                        }) => {
-                            self.event_handler.handle(
-                                &Event::WindowResize(Rectangle { width, height }),
-                                &mut self.state,
-                                &mut self.graphics,
-                            );
-                        }
-                        _ => (),
                     }
+                    _ => (),
                 }
-                _ => (),
             })
             .unwrap();
     }
