bad9ad8e3e1bb17b0d16006dcde65ffb5c3b54ac
TGElder <tgelder@gmail.com>
2024-11-07 21:05:50 +0000

    feat (mountain): pistes can be opened/closed (#477)

--- mountain/src/controllers/entity_window.rs
+++ mountain/src/controllers/entity_window.rs
@@ -5,12 +5,14 @@ use commons::geometry::{xy, XY, XYZ};
 use crate::controllers::Result::{self, Action, NoAction};
 use crate::model::gate::Gate;
 use crate::model::lift::Lift;
+use crate::model::piste::Piste;
 use crate::widgets::entity_window::EntityWindow;
 
 pub struct Parameters<'a> {
     pub mouse_xy: &'a Option<XY<u32>>,
     pub lifts: &'a HashMap<usize, Lift>,
     pub gates: &'a HashMap<usize, Gate>,
+    pub pistes: &'a HashMap<usize, Piste>,
     pub windows: &'a mut HashMap<usize, EntityWindow>,
     pub graphics: &'a mut dyn engine::graphics::Graphics,
 }
@@ -20,6 +22,7 @@ pub fn trigger(
         mouse_xy,
         lifts,
         gates,
+        pistes,
         windows,
         graphics,
     }: Parameters<'_>,
@@ -43,6 +46,13 @@ pub fn trigger(
     for (gate_id, gate) in gates.iter() {
         if gate.footprint.contains(position) {
             windows.insert(*gate_id, EntityWindow::new(*gate_id, *mouse_xy));
+            return Action;
+        }
+    }
+
+    for (piste_id, piste) in pistes.iter() {
+        if piste.grid.in_bounds(position) && piste.grid[position] {
+            windows.insert(*piste_id, EntityWindow::new(*piste_id, *mouse_xy));
             return Action;
         }
     }
