cd2375b32427ab74857a8b9ee3d5d85a9e121fcb
Thomas Elder <tgelder@gmail.com>
2024-12-04 21:00:41 +0000

    feat (mountain): replaced target_scrubber with target_checker

--- mountain/src/systems/target_scrubber.rs
+++
@@ -1,9 +0,0 @@
-use std::collections::HashMap;
-
-use commons::map::ContainsKeyValue;
-
-use crate::model::open;
-
-pub fn run(open: &HashMap<usize, open::Status>, targets: &mut HashMap<usize, usize>) {
-    targets.retain(|_, target| open.contains_key_value(target, open::Status::Open))
-}
