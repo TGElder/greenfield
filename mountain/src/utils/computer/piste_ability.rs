8dbcee7d3f9a8a3433f38761d6b0eef3e85a08fe
TGElder <tgelder@gmail.com>
2025-03-12 17:28:31 +0000

    fix (mountain): redrawing piste when ability changes (#523)

--- mountain/src/utils/computer/piste_ability.rs
+++ mountain/src/utils/computer/piste_ability.rs
@@ -4,20 +4,26 @@ use crate::model::ability::Ability;
 use crate::model::costs::Costs;
 use crate::model::entrance::Entrance;
 use crate::model::exit::Exit;
+use crate::model::piste::Piste;
 use crate::model::skiing::State;
+use crate::systems::terrain_artist;
 
 pub fn compute_piste(
     piste_id: &usize,
+    pistes: &HashMap<usize, Piste>,
     costs: &HashMap<usize, Costs<State>>,
     entrances: &HashMap<usize, Entrance>,
     exits: &HashMap<usize, Exit>,
     abilities: &mut HashMap<usize, Ability>,
+    terrain_artist: &mut terrain_artist::System,
 ) {
     let Some(costs) = costs.get(piste_id) else {
         return;
     };
 
-    abilities.remove(piste_id);
+    let Some(piste) = pistes.get(piste_id) else {
+        return;
+    };
 
     let entrances = entrances
         .values()
@@ -41,6 +47,15 @@ pub fn compute_piste(
         )
         .map(|(exit_id, _)| exit_id)
         .collect::<Vec<_>>();
+
+    let old_ability = abilities.remove(piste_id);
+    let new_ability = compute_ability(costs, &entrances, &exit_ids);
+
+    if old_ability != new_ability {
+        if let Ok(rectangle) = piste.grid.rectangle() {
+            terrain_artist.update_overlay(rectangle);
+        }
+    }
 
     if let Some(ability) = compute_ability(costs, &entrances, &exit_ids) {
         abilities.insert(*piste_id, ability);
